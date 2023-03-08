9a0dd89a3308dd481651688e4b994e961915c8e8
Telefragged <vetle.braend@gmail.com>
2023-03-08 22:07:04 +0100

    Pin Rust to stable 1.67.1

--- shell.nix
+++ shell.nix
@@ -3,9 +3,9 @@ let
   rust_overlay = import (builtins.fetchTarball https://github.com/oxalica/rust-overlay/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
 
-  rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+  rust = pkgs.rust-bin.stable."1.67.1".default.override {
     extensions = [ "rust-src" "clippy" ];
-  });
+  };
 in pkgs.mkShell {
   buildInputs = with pkgs; [
     rust
